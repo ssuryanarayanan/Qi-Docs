a1254ccde3fdaf941b051d0abc29eae3c7f9c337
chad-at-osisoft <chad@osisoft.com>
2015-08-28 16:32:51 -0700

    Migrating from other repository.

---
+++ Samples/Python/QiPy/qipy.py
@@ -0,0 +1,10 @@
+﻿# qipy.py
+# chad@osisoft.com
+
+from QiClient import QiClient
+from QiType import QiType
+from QiTypeCode import QiTypeCode
+from QiTypeProperty import QiTypeProperty
+from QiStream import QiStream
+from QiError import QiError
+
