f7ad7ddbba238a8c522c49157766a14715b1beed
chad-at-osisoft <chad@osisoft.com>
2016-03-31 09:46:30 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -26,9 +26,8 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-#extensions = [
-#    'sphinx.ext.intersphinx',
-#    'sphinxcontrib.dotnetdomain']
+extensions = [
+    'sphinx.ext.intersphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates', '../common/_templates']
